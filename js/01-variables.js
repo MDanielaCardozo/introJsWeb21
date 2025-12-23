932eb2fd65e8d9141af63fc88cbc36d34b332937
Daniela Cardozo <mdanielacardozo@gmail.com>
2025-12-23 11:38:07 -0300

    agregue estructura inicial y variables

---
+++ js/01-variables.js
@@ -0,0 +1,21 @@
+// Esto es un comentario de una linea
+
+/* 
+Esto es un comentario
+de varias
+lineas
+ */
+
+//mandar mensaje en la consola
+console.log("Hola mundo");
+console.info("Hola mundo");
+console.warn("Hola mundo");
+console.error("Hola mundo");
+
+//como mostrar en html
+document.writeln("Hola mundo")
+document.writeln("<h3>Hola mundo</h3>");
+
+alert("Esto es una ventana emergente alert")
+
+
