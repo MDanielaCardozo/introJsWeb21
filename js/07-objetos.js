3df02bbd158467ce83432781891d8444a5bfb17a
Daniela Cardozo <mdanielacardozo@gmail.com>
2026-01-22 11:20:31 -0300

    agregue archivo objeto.js

---
+++ js/07-objetos.js
@@ -0,0 +1,31 @@
+//OBJETO 
+//declar un objeto con notacion literal
+
+/* Propiedad
+codigo: 2334455 (NUMBER)
+nombre: "Zapatillas deportivas" (STRING)
+tamaño
+precio
+marca
+peso
+estado: true(tipo de dato boleano)
+metodos:
+actualizarPrecio(nuevoPrecio)
+cambiarEstado(nuevoEstado)
+*/
+
+//OBJETO VACIO
+const cancion = {};
+
+//OBJETO CON VALORES
+const funko = {
+    //definir propiedades
+    //clave/key : valor/value
+    codigo: 867,
+    precio: 20.5,
+    nombre: "Tanjiro kamado",
+    marca: "Special Edition",
+    estado: true,
+    //metodos
+    
+}
